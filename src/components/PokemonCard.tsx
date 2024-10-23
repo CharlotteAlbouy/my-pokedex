8beb4c1dd99c8b33d86a172fc8f948a10e90b63e
CharlotteAlbouy <albouy.charlotte@gmail.com>
2024-10-23 17:27:02 +0200

    component2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 
+const pokemonList = [
+	{
+		name: "Bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+
+	{
+		name: "Mew",
+	},
+];
+
 const PokemonCard: React.FC = () => {
+	const pokemon = pokemonList[0];
 	return (
 		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="Bulbasaur"
-			/>
-			<figcaption>Bulbasaur</figcaption>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
 };
